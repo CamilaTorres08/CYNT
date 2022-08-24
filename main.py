e4743ea6df693c1b0bb1e5c6a43fdf3bba309c33
CamilaTorres08 <111332434+CamilaTorres08@users.noreply.github.com>
2022-08-24 11:07:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+#Calculadora de numeros complejos
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def SumComplex (numero1,numero2): 
+    real = numero1[0]+numero2[0]
+    imag = numero1[1]+numero2[1]
+    return (real,imag)
+def MultComplex (numero1,numero2):
+    real = numero1[0]*numero2[0] - numero1[1]*numero2[1]
+    imag = numero1[0]*numero2[1] + numero1[1]*numero2[0]
+    return (real,imag)
+def RestComplex (numero1,numero2):
+    real = numero1[0]- numero2[0]
+    imag = numero1[1] - numero2[1]
+    return (real,imag)
+def DivComplex (numero1,numero2):
+    result1 = numero1[0]*numero2[0]+numero1[1]*numero2[1]
+    result2 = (numero2[0]**2)+(numero2[1]**2)
+    result3 = numero2[0]*numero1[1] - numero1[0]*numero2[1]
+    return (result1//result2,result3//result2)
+def ModComplex (numero):
+    a = numero[0]**2
+    b = numero[1]**2
+    return math.sqrt(a+b)
+def ConjComplex (numero):
+    return (numero[0],numero[1]*-1)
+def PolCartComplex (numero): #Convierte de polar a cartesiano
+    a = numero[0]*math.cos(numero[1])
+    b = numero[0]*math.sin(numero[1])
+    return (a,b)
+def CartPolComplex (numero): #Convierte de cartesiano a polar
+    p = ModComplex(numero)
+    ang = FaseComplex(numero)
+    return (p,ang)
+def FaseComplex (numero): #Halla la fase del complejo
+    return math.atan2(numero[1],numero[0])
+def PrintNumsCart (c): #Imprime numeros cartesianos
+    real = str(c[0])
+    img = str(c[1])
+    if c[1] < 0:
+        print(real,img,"i",sep="",end='\n')
+    else: 
+        print(real,"+",img,"i",sep="",end='\n')
+def PrintNumsPol (c): #Inprime numeros polares
+    print(str(c[0]),"e^(i" ,str(c[1]),")")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Suma:", end= " ")        
+PrintNumsCart(SumComplex((2,3),(4,7)))
+print("Multiplicación:", end= ' ')
+PrintNumsCart(MultComplex((2,3),(4,7)))
+print("Resta:",end= ' ')
+PrintNumsCart(RestComplex((3,-1),(1,4)))
+print("División:",end= ' ')
+PrintNumsCart(DivComplex((-2,1),(1,2)))
+print("Módulo:",end= ' ')
+print(ModComplex((1,-1)))
+print("Conjugado:",end= ' ')
+PrintNumsCart(ConjComplex((3,2)))
+print("Polar a cartesiano:",end= ' ')
+PrintNumsCart(PolCartComplex((3,-1)))
+print("Cartesiano a polar:",end= ' ')
+PrintNumsPol(CartPolComplex((-3,-2)))
+print("Fase:",end= ' ')
+print(FaseComplex((-3,-2)))
